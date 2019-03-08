3465546413d02625658e2eea8473edd652fde013
Chris Ball <chrisball7@gmail.com>
2019-03-08 08:04:41 -0800

    added user Model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime as dt
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from passlib.hash import sha256_crypt
 from . import app
 
 
@@ -37,3 +38,34 @@ class Portfolio(db.Model):
 
     def __repr__(self):
         return '<Portfolio {}>'.format(self.portfolio_name)
+
+
+class User(db.Model):
+    """
+
+    """
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    portfolios = db.relationship('Portfolio', backref='user', lazy=True)
+    date_created = db.Column(db.DateTime, default=dt.now())
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.hash = sha256_crypt.hash(password)
+
+    @classmethod
+    def check_password_hash(cls, user, password):
+        """
+
+        """
+        if user is not None:
+            if sha256_crypt.verify(password, user.hash):
+                return True
+
+        return False
