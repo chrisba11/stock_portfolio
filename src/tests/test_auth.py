0440bba23b55ab6d945ec9510271dd71f08e3317
Chris Ball <chrisball7@gmail.com>
2019-03-13 16:08:37 -0700

    added test cb env stuffs

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -5,16 +5,25 @@
 import pytest
 
 
+# @pytest.mark.skip()
 def test_get_register_status(client):
     res = client.get('/register')
     assert res.status_code == 201
 
 
+# @pytest.mark.skip()
 def test_get_register_has_correct_text(client):
     res = client.get('/register')
     assert b'<title>REG</title>' in res.data
 
 
+# @pytest.mark.skip()
 def test_has_correct_nav_when_not_logged_in(client):
     res = client.get('/')
-    assert b''
+    assert b'<a href="/"' in res.data
+    assert b'<a href="/register"' in res.data
+    assert b'<a href="/login"' in res.data
+    assert b'<a href="/logout"' in res.data
+
+
+# def test_register_post
